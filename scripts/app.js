b35a1a697aa112884e5f2e829ae6ab333b6271a0
Sami Samara <samisamara99@yahoo.com>
2022-05-28 02:35:36 -0500

    added search result display feature. Users can now see results of their searches

--- scripts/app.js
+++ scripts/app.js
@@ -1,4 +1,27 @@
 const cityForm = document.querySelector('form');
+const card = document.querySelector('.card');
+const details = document.querySelector('.details');
+
+const updateUI = (data) => {
+    // making a variable = the cityDets and weather properties we passed from updateCity
+    const cityDets = data.cityDets;
+    const weather = data.weather;
+
+    //update details template
+    details.innerHTML = `
+        <h5 class="my-3">${cityDets.EnglishName}</h5>
+        <div class="my-3">${weather.WeatherText}</div>
+        <div class="display-4 my-4">
+            <span>${weather.Temperature.Imperial.Value}</span>
+            <span>&deg;F</span>
+    `;
+
+    // remove the d-none class if present
+    if (card.classList.contains('d-none')) {
+        card.classList.remove('d-none');
+    }
+
+};
 
 const updateCity = async (city) => {
     
@@ -32,6 +55,6 @@ cityForm.addEventListener('submit', e => {
     // update the ui with new city
     // updateCity will return the object as the data, so we call the result from the function "data"
     updateCity(city)
-        .then(data => console.log(data))
+        .then(data => updateUI(data))
         .catch(err => console.log(err));
 })
