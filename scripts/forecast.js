719a76d1fe52be46186cf1c4766a44a87e19b0d8
Sami Samara <samisamara99@yahoo.com>
2022-05-28 01:12:50 -0500

    added skeletal structure of weather app

---
+++ scripts/forecast.js
@@ -0,0 +1,11 @@
+// this key is needed from AccuWeather.com in order to request weather information
+const key = 'OZmlatqgr1gEHdxvKRDTwsWv7LGdR36o';
+
+// async function to get city key info
+const getCity = async () => {
+    // link to city search api
+    const base = 'http://dataservice.accuweather.com/locations/v1/cities/search'
+}
+
+
+// http://dataservice.accuweather.com/currentconditions/v1/{locationKey}
